8f75647f8b7c992427ffe7b2a24c69467a81a7a7
Ginger Dudley <gingerdudley@Gingers-MacBook-Pro.local>
2019-02-16 16:50:41 -0800

    adding image picker

---
+++ app3/UploadViewController.m
@@ -0,0 +1,32 @@
+//
+//  UploadViewController.m
+//  app3
+//
+//  Created by Ginger Dudley on 2/16/19.
+//  Copyright © 2019 Ginger Dudley. All rights reserved.
+//
+
+#import "UploadViewController.h"
+
+@interface UploadViewController ()
+
+@end
+
+@implementation UploadViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
